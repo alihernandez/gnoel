33acc38bb9dd619b89efeb225d8bf96f7b535ae4
phantomramen <ali.hernandez9two@gmail.com>
2022-02-12 16:55:51 -0700

    created the controller

---
+++ backend/node_app/controllers/blog.controller.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+const Blog = db.blogs;
+// Create and Save a new blog
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Blogs from the database
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Blog by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a Blog with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all Blogs from the database
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published Blogs
+exports.findAllPublished = (req, res) => {
+
+};
