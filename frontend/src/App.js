dd92cb9de4244551a19a18edd487e75e392b93b5
phantomramen <ali.hernandez9two@gmail.com>
2022-03-15 21:37:32 -0600

    added react pages for authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AddBlog from "./components/AddBlog";
 import BlogList from "./components/BlogList";
 import Blog from "./components/Blog";
 import "./App.css";
+
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+
+import { logout } from "./actions/auth";
+import { clearMessage } from "./actions/message";
+import { history } from "./helpers/history";
 
 function App() {
     return (
@@ -24,12 +33,30 @@ function App() {
                 Add
               </Link>
             </li>
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Register
+              </Link>
+              </li>
+              <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                Profile
+              </Link>
+            </li>
           </div>
         </nav>
         <div className="container mt-3">
           <Routes>
             <Route path="/" exact element={<BlogList />} />
             <Route exact path="/add" element={<AddBlog />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/profile" element={<Profile />} />
             <Route path="/blogs/:id" exact element={<Blog />} />
           </Routes>
         </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import store from "./store";
 import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(
   <HashRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
   </HashRouter>,
   document.getElementById("root")
 );
