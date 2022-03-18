b436c0acebb8692fb7c9423341e15ee93d4defd9
phantomramen <ali.hernandez9two@gmail.com>
2022-03-18 16:54:31 -0600

    env heroku

--- node_app/config/db.config.js
+++ node_app/config/db.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    uri: process.env.MONGODB_URI
+    url: process.env.MONGODB_URI
 };
-
-// mongodb+srv://ahernDB:DB80808@gpcluster.jg8tz.mongodb.net/gnoel?retryWrites=true&w=majority
