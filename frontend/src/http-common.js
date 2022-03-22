35ea41aa5c9b03ea02aebc2e7174ec3600505147
phantomramen <ali.hernandez9two@gmail.com>
2022-03-21 20:19:45 -0600

    heroku error

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "salty-refuge.herokuapp.com/api",
+    baseURL: "https://salty-refuge-49052.herokuapp.com/api",
     headers: {
         "Content-type": "application/json"
     }
